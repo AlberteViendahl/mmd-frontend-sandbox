5bcdece5b29567d5d8b3c1ed1018fee21e7cb2ec
Alberte <alan0002@stud.kea.dk>
2024-11-01 08:59:35 +0100

    friday

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -1,17 +1,20 @@
-
 import { RxCookie } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
 import Button from "./Button";
 
 const Cookie = () => {
-    return ( <div className="baggrund">
-        <div className="flex">   <RxCookie className="cookie" />
-        <RxCross1 className="kryds"/>
-        </div>
-       
-<p>We used Cookie to improve your user experience.</p>
-<Button/>
-    </div> );
-}
- 
+  return (
+    <div className="baggrund dark:bg-white">
+      <div className="flex">
+        {" "}
+        <RxCookie className="cookie" />
+        <RxCross1 className="kryds" />
+      </div>
+
+      <p>We used Cookie to improve your user experience.</p>
+      <Button />
+    </div>
+  );
+};
+
 export default Cookie;
